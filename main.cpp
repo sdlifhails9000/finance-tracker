25705f7dae9948a13e5252c452281662b6b01469
Agha Waleed Hassan <ebicandawesome9000@gmail.com>
2026-01-16 19:05:39 +0500

    Implemented `edit_category`

--- main.cpp
+++ main.cpp
@@ -463,6 +463,7 @@ void add_category(sqlite3*mdb, int user_id){
                 cout << "Name too big (> 50)" << endl;
             }
         } while (category_name.size() > 50);
+
         char want_budget;
         want_budget = input<char>("Do you want to set a budget for this category (Y/n)? (Optional)");
         want_budget = tolower(want_budget);
@@ -509,8 +510,66 @@ void add_category(sqlite3*mdb, int user_id){
     return;    
 }
 
-void edit_category(sqlite3*mdb, int user_id, vector<int> category_ids){
-    return;
+void edit_category(sqlite3 *mdb, vector<int> category_ids){
+    sqlite3_stmt *stmt;
+    const char *stmt_str =
+        "UPDATE categories"
+        "SET name = ?, budget_amount = ? "
+        "WHERE id = ?";
+
+    int selection;
+    while (true) {
+        selection = input<int>("Enter the category you wanna edit: ");
+        if (find(category_ids.begin(), category_ids.end(), selection) == category_ids.end()) {
+            cout << "Invalid ID!" << endl;
+        } else {
+            break;
+        }
+    }
+
+    sqlite3_prepare_v2(mdb, stmt_str, -1, &stmt, nullptr);
+
+    string name;
+    double budget_amount = 0;
+
+    while (true) {
+        do {
+            name = input<string>("Enter new name (at most 50 characters): ");
+            if (name.size() != 0) {
+                cout << "Name can't be empty" << endl;
+            } else if (name.size() > 50) {
+                cout << "Name too big. Should be at most 50 character" << endl;
+            }
+        } while (name.size() != 0 && name.size() > 50);
+
+        do {
+            budget_amount = input<double>("Enter budget amount (zero for no budget): ");
+            if (budget_amount < 0) {
+                cout << "Budget can't be negative" << endl;
+            }
+        } while (budget_amount < 0);
+
+        sqlite3_clear_bindings(stmt);
+        sqlite3_bind_text(stmt, 1, name.c_str(), -1, SQLITE_STATIC);
+        sqlite3_bind_double(stmt, 2, budget_amount);
+        sqlite3_bind_int(stmt, 3, selection);
+
+        int rc = sqlite3_step(stmt);
+        if (rc == SQLITE_CONSTRAINT) {
+            int extended = sqlite3_extended_errcode(mdb);
+            if (extended == SQLITE_CONSTRAINT_UNIQUE) {
+                cout << "Name already exists!" << endl;
+                sqlite3_reset(stmt);
+                continue;
+            } else {
+                cout << sqlite3_errmsg(mdb) << endl;
+                exit(EXIT_FAILURE);
+            }
+        } else if (rc != SQLITE_DONE) {
+            cout << sqlite3_errmsg(mdb) << endl;
+            exit(EXIT_FAILURE);
+        }
+    }
 }
 
 
@@ -569,7 +628,7 @@ void category_UI(sqlite3* mdb, int user_id){
             break;
 
         case 2:
-            edit_category(mdb, user_id, category_ids);   //max_id is to be passed so that we have the upper limit of VALID CHOICES (improves efficiency so we dont have to reconstruct it in edit_transaction())
+            edit_category(mdb, category_ids);   //max_id is to be passed so that we have the upper limit of VALID CHOICES (improves efficiency so we dont have to reconstruct it in edit_transaction())
             break;
         default:
             cout << "Call kru bacha?" << endl;
